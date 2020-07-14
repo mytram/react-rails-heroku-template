93fda9a9702d150b07023efecad1a461230590ac
mytram <mytram2@gmail.com>
2020-07-14 21:51:21 +1000

    Use /api/versions/0 in the frontend webapp

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+type Version = {
+  version: string;
+  name: string;
+};
 
 function App() {
+  const [version, setVersion] = useState<Version>();
+
+  useEffect(() => {
+    fetch("/api/versions/0", {
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => response.json())
+      .then((payload: { data: Version }) => setVersion(payload.data));
+  }, []);
+
+  if (!version) {
+    return null;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Version: {version.version}</p>
+        <p>Name: {version.name}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
